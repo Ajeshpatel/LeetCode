36a46cc660d311350f95e0251ea473f3313c43d6
Ajesh patel <ajeshpatel10@gmail.com>
2026-06-05 21:50:12 +0530

    singleNumber

---
+++ easy/SingleNumber.java
@@ -0,0 +1,58 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,2,1]
+
+// Output: 1
+
+// Example 2:
+
+// Input: nums = [4,1,2,1,2]
+
+// Output: 4
+
+// Example 3:
+
+// Input: nums = [1]
+
+// Output: 1
+
+import java.util.*;
+public class SingleNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter size of arr: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter Array Element: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Temperatures Increase: ");
+        System.out.println(singleNumber(arr));
+
+        sc.close();
+
+    }
+
+    public static int singleNumber(int[] nums) {
+
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+
+        return result;
+
+    }
+}
