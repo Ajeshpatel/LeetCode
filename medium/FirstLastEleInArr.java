fcabc713817d6ea9aff08ff911b92bf88501c2e5
Ajesh patel <ajeshpatel10@gmail.com>
2026-06-16 19:24:05 +0530

    First & Last Ele. in Sorted Arr

---
+++ medium/FirstLastEleInArr.java
@@ -0,0 +1,65 @@
+// Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+// If target is not found in the array, return [-1, -1].
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+
+// Example 2:
+// Input: nums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+
+// Example 3:
+// Input: nums = [], target = 0
+// Output: [-1,-1]
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FirstLastEleInArr {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter size of arr: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter Array Element: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter Target Element: ");
+        int target = sc.nextInt();
+
+        int printing[] = searchRange(arr, target);
+        System.out.println(Arrays.toString(printing));
+
+        sc.close();
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+
+        int[] result = { -1, -1 };
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (result[0] == -1) {
+                    result[0] = i;
+                }
+                result[1] = i;
+            }
+        }
+
+        return result;
+    }
+    
+}
