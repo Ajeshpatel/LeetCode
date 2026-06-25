2d572225f93de4d22bc820c03f4544215529c65b
Ajesh patel <ajeshpatel10@gmail.com>
2026-06-25 22:21:07 +0530

    BestTimeToByAndSl Bruit force update

--- easy/BestTimeToByAndSl.java
+++ easy/BestTimeToByAndSl.java
@@ -61,5 +61,17 @@ public class BestTimeToByAndSl {
 
         return maxProfit;
     }
+
+    // int maxProfit = 0;
+
+    // for(int i = 0; i<prices.length-1; i++) {
+    //     for (int j = i+1; j<prices.length; j++) {
+    //             int profit = prices[j] - prices[i];
+
+    //             maxProfit = Math.max(maxProfit, profit);
+    //     }
+    // }
+
+    // return maxProfit;
   
 }
