298997a4d3a30f6479234c6343ec9de35695f8fd
Ajesh patel <ajeshpatel10@gmail.com>
2026-07-03 15:32:19 +0530

    Frequency of Each Element

---
+++ easy/FreqOfEachNum.java
@@ -0,0 +1,40 @@
+
+
+
+// 
+
+/**
+ * FreqOfEachNum
+ */
+import java.util.*;
+public class FreqOfEachNum {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size of an array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter Array Ele");
+
+        for (int i = 0; i<arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // if want sorted then use TreeMap
+
+        for (int i = 0; i<arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        System.out.println(map);
+
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+
+
+}
