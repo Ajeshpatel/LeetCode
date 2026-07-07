8b3488dd392fe88b6168400db1ef9028a5da7abe
Ajesh patel <ajeshpatel10@gmail.com>
2026-07-07 19:30:00 +0530

    Group Anagram

---
+++ medium/GroupAnagram.java
@@ -0,0 +1,80 @@
+// Given an array of strings strs, group the together. You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// Explanation:
+
+//     There is no string in strs that can be rearranged to form "bat".
+//     The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+//     The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+// Example 2:
+
+// Input: strs = [""]
+
+// Output: [[""]]
+
+// Example 3:
+
+// Input: strs = ["a"]
+
+// Output: [["a"]]
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * GroupAnagram
+ */
+public class GroupAnagram {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter size of arr: ");
+        int n = sc.nextInt();
+
+        String arr[] = new String[n];
+
+        System.out.println("Enter Array Element: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.next();
+        }
+
+        System.out.print("Maximum Product SubArray: ");
+        System.out.println(groupAnagrams(arr));
+
+        sc.close();
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String s: strs) {
+
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+
+            String key = new String(arr);
+
+            if(!map.containsKey(key)) {
+                map.put(key, new ArrayList<String>());
+            }
+            map.get(key).add(s);
+
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
