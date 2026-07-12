45f8a91054785f65b3efdb4a19e7928f25960038
Ajesh patel <ajeshpatel10@gmail.com>
2026-07-12 23:37:03 +0530

    code rearange majority Ele

--- easy/MajoriryElement.java
+++ easy/MajoriryElement.java
@@ -3,8 +3,6 @@
 // The majority element is the element that appears more than ⌊n / 2⌋ times.
 //  You may assume that the majority element always exists in the array.
 
- 
-
 // Example 1:
 
 // Input: nums = [3,2,3]
@@ -100,7 +98,5 @@ class MajoriryElement {
         }
 
         return ans;
- 
     }
-    
 }
